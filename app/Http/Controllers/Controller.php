8b04e8edd8dd0c27cc798a39b2df5d54c1f1a7eb
Elsayed Ahmed Elwasefy <elsayed@parent.app>
2026-02-28 02:22:46 +0200

    Update base controller with post & get responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 abstract class Controller
 {
-    //
+    public function postJsonSuccessResponse($data = []): JsonResponse
+    {
+        return response()->json($data, Response::HTTP_CREATED);
+    }
+
+    public function getJsonSuccessResponse($data = []): JsonResponse
+    {
+        return response()->json($data, Response::HTTP_OK);
+    }
 }
